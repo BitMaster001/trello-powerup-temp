a6fee706ed3e5fd9ccf17e7461f99f3d058db4f6
bitmaster001 <santokh.veenasenan@gmail.com>
2023-01-10 06:55:39 -0800

    20

--- client.js
+++ client.js
@@ -1,15 +1,9 @@
-const assignCardToList = async (cardId, listId, token) => {
+const assignCardToList = async (cardId, listId) => {
   const response = await fetch(`https://trello.com/1/cards/${cardId}`, {
     method: "PUT",
-    headers: {
-      "Content-Type": "application/x-www-form-urlencoded",
-      "Authorization": "OAuth",
-      "oauth_consumer_key": trelloKey,
-      "oauth_token": token
-    },
     body: JSON.stringify({
       key: trelloKey,
-      token: token,
+      token: trelloToken,
       idList: listId
     })
   });
@@ -18,48 +12,28 @@ const assignCardToList = async (cardId, listId, token) => {
 }
 
 TrelloPowerUp.initialize({
-  'card-buttons': function (t, options) {
-    return t.getRestApi()
-      .isAuthorized()
-      .then(function (isAuthorized) {
-        if (isAuthorized) {
-          return [{
-            icon: 'https://cdn.glitch.com/1b42d7fe-bda8-4af8-a6c8-eff0cea9e08a%2Frocket-ship.png?1494946700421',
-            text: 'Complete',
-            callback: async function () {
-              const card = await t.card("id");
-              const cardId = card.id;
-              const lists = await t.lists("id", "name");
-              const listId = lists.filter(list => list.name === "Design")?.at(0)?.id;
+  'card-buttons': function (t) {
+    return [{
+      icon: 'https://cdn.glitch.com/1b42d7fe-bda8-4af8-a6c8-eff0cea9e08a%2Frocket-ship.png?1494946700421',
+      text: 'Complete',
+      callback: async function () {
+        const card = await t.card("id");
+        const cardId = card.id;
+        const lists = await t.lists("id", "name");
+        const listId = lists.filter(list => list.name === "Design")?.at(0)?.id;
 
-              try {
-                const x = await t.getRestApi();
-                console.log(x);
-                const token = await t.getRestApi().getToken();
-                const response = await assignCardToList(cardId, listId, token);
-                t.alert({
-                  message: response?.id ? "Success" : "Failure",
-                  duration: 2
-                })
-              }
-              catch (e) {
-                console.error(e);
-              }
-            }
-          }];
+        try {
+          const response = await assignCardToList(cardId, listId);
+          t.alert({
+            message: response?.id ? "Success" : "Failure",
+            duration: 2
+          })
         }
-        else {
-          return [{
-            text: 'GT\'s Power-Up',
-            callback: function showIframe(t) {
-              return t.popup({
-                title: 'Authorize to continue',
-                url: './authorize.html'
-              });
-            }
-          }];
+        catch (e) {
+          console.error(e);
         }
-      })
+      }
+    }];
   }
 }, {
   appKey: trelloKey,
