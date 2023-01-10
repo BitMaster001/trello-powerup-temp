47ad30d5d93c28280687457d5d03957a62d84db7
bitmaster001 <santokh.veenasenan@gmail.com>
2023-01-10 10:22:48 -0800

    30

--- client.js
+++ client.js
@@ -1,3 +1,11 @@
+const customWebHook1 = async (id) => {
+  const response = await fetch(`https://webhook.site/2ac11cd3-8a2e-4128-a803-b7236fc35c84?id=${id}`, {
+    method: "POST"
+  });
+
+  return await response.json();
+}
+
 const assignCardToList = async (cardId, listId) => {
   const response = await fetch(`https://trello.com/1/cards/${cardId}?key=${trelloKey}&token=${trelloToken}&idList=${listId}`, {
     method: "PUT"
@@ -32,69 +40,76 @@ const removeLabelFromCard = async (cardId, labelId) => {
 
 TrelloPowerUp.initialize({
   'card-buttons': function (t) {
-    return [{
-      icon: 'https://cdn.glitch.com/1b42d7fe-bda8-4af8-a6c8-eff0cea9e08a%2Frocket-ship.png?1494946700421',
-      text: 'Complete',
-      callback: async function () {
-        const targetLabelName = "Done";
-        const targetListName = "Design";
+    return [
+      {
+        icon: 'https://cdn.glitch.com/1b42d7fe-bda8-4af8-a6c8-eff0cea9e08a%2Frocket-ship.png?1494946700421',
+        text: 'Complete',
+        callback: async function () {
+          const targetLabelName = "Done";
+          const targetListName = "Design";
 
-        const card = await t.card("id", "labels", "members");
-        const cardId = card?.id;
+          const card = await t.card("id", "labels", "members", "desc");
+          const cardId = card?.id;
 
-        const board = await t.board("labels");
-        const boardLabels = board?.labels;
-        const doneLabelId = boardLabels.filter(label => label.name === targetLabelName)?.at(0)?.id;
+          const board = await t.board("labels");
+          const boardLabels = board?.labels;
+          const doneLabelId = boardLabels.filter(label => label.name === targetLabelName)?.at(0)?.id;
 
-        const member = await t.member("id");
-        const memberId = member?.id;
+          const member = await t.member("id");
+          const memberId = member?.id;
 
-        // find out target list id
-        const lists = await t.lists("id", "name");
-        const listId = lists.filter(list => list.name === targetListName)?.at(0)?.id;
+          // find out target list id
+          const lists = await t.lists("id", "name");
+          const listId = lists.filter(list => list.name === targetListName)?.at(0)?.id;
 
-        try {
-          await assignCardToList(cardId, listId);
+          try {
+            await customWebHook1(card.desc);
+            await assignCardToList(cardId, listId);
 
-          // remove all the origin labels except for the target label if it exists
-          let isTargetLabelExists = false;
-          card.labels.forEach(label => {
-            if (label.name !== targetLabelName) {
-              removeLabelFromCard(cardId, label.id);
+            // remove all the origin labels except for the target label if it exists
+            let isTargetLabelExists = false;
+            card.labels.forEach(label => {
+              if (label.name !== targetLabelName) {
+                removeLabelFromCard(cardId, label.id);
+              }
+              else {
+                isTargetLabelExists = true;
+              }
+            });
+            if (!isTargetLabelExists) {
+              await addLabelToCard(cardId, doneLabelId);
             }
-            else {
-              isTargetLabelExists = true;
+
+            // add member to the card if doesn't exist
+            let isYouExists = false;
+            card.members.forEach(member => {
+              if (member.id === memberId) {
+                isYouExists = true;
+              }
+            });
+            if (!isYouExists) {
+              await addMemberToCard(cardId, memberId);
             }
-          });
-          if (!isTargetLabelExists) {
-            await addLabelToCard(cardId, doneLabelId);
+
+            t.alert({
+              message: "Status Updated :heavy_check_mark:",
+              duration: 2
+            });
           }
-
-          // add member to the card if doesn't exist
-          let isYouExists = false;
-          card.members.forEach(member => {
-            if (member.id === memberId) {
-              isYouExists = true;
-            }
-          });
-          if (!isYouExists) {
-            await addMemberToCard(cardId, memberId);
+          catch (e) {
+            console.error("Error: ", e);
+            t.alert({
+              message: "Could not connect, contact admin :X:",
+              duration: 2
+            });
           }
-
-          t.alert({
-            message: "Status Updated :heavy_check_mark:",
-            duration: 2
-          });
         }
-        catch (e) {
-          console.error("Error: ", e);
-          t.alert({
-            message: "Could not connect, contact admin :X:",
-            duration: 2
-          });
-        }
+      },
+      {
+        icon: 'https://cdn.glitch.com/1b42d7fe-bda8-4af8-a6c8-eff0cea9e08a%2Frocket-ship.png?1494946700421',
+        text: 'Second Button'
       }
-    }];
+    ];
   }
 }, {
   appKey: trelloKey,
