736ee0d8bd630096112ae01e3c4e560a7521ab36
bitmaster001 <santokh.veenasenan@gmail.com>
2023-01-10 07:28:08 -0800

    24

--- client.js
+++ client.js
@@ -6,6 +6,22 @@ const assignCardToList = async (cardId, listId) => {
   return await response.json();
 }
 
+const getLabelsOnTheBoard = async (boardId) => {
+  const response = await fetch(`https://trello.com/1/boards/${boardId}/labels?key=${trelloKey}&token=${trelloToken}`, {
+    method: "GET"
+  });
+
+  return await response.json();
+}
+
+const addMemberToCard = async (cardId, memberId) => {
+  const response = await fetch(`https://trello.com/1/cards/${cardId}?key=${trelloKey}&token=${trelloToken}&idMembers=${memberId}`, {
+    method: "POST"
+  });
+
+  return await response.json();
+}
+
 const addLabelsToCard = async (cardId, labelId) => {
   const response = await fetch(`https://trello.com/1/cards/${cardId}?key=${trelloKey}&token=${trelloToken}&idLabels=${labelId}`, {
     method: "POST"
@@ -20,15 +36,19 @@ TrelloPowerUp.initialize({
       icon: 'https://cdn.glitch.com/1b42d7fe-bda8-4af8-a6c8-eff0cea9e08a%2Frocket-ship.png?1494946700421',
       text: 'Complete',
       callback: async function () {
-        const card = await t.card("id", "labels", "members");
-        console.log("🚀 ~ file: client.js:16 ~ card", JSON.stringify(card));
-        const cardId = card.id;
+        const card = await t.card("id");
+        const cardId = card?.id;
+        const board = await t.board("id", "labels");
+        console.log("🚀 ~ file: client.js:34 ~ board", board);
+        const boardId = board?.id;
+        const boardLabels = board?.labels;
 
-        const cardLabels = card.labels;
-        // const labelId = cardLabels.filter(label => label.)
+        const member = await t.member("id");
+        console.log("🚀 ~ file: client.js:47 ~ member", member);
+        const memberId = member?.id;
 
-        const cardMembers = card.members;
-        
+        // const labels = await getLabelsOnTheBoard(boardId);
+
         const lists = await t.lists("id", "name");
         const listId = lists.filter(list => list.name === "Design")?.at(0)?.id;
 
